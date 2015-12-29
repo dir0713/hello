c27e78e98bd0f56ae5a73dbeab929efdb41dc963
coder-huyw <hayden-dir@outlook.com>
2015-12-29 12:05:35 +0800

    Initial commit

---
+++ app/src/main/java/com/huyw/hello/MainActivity.java
@@ -0,0 +1,13 @@
+package com.huyw.hello;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
